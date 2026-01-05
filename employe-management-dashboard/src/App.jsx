00f2da32e97d3b38de9c4ff229bd7d343950c58d
swathicherala <swathicherala96@gmail.com>
2026-01-05 17:45:19 +0530

    protected route

--- employe-management-dashboard/src/App.jsx
+++ employe-management-dashboard/src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Dashboard from "./pages/Dashboard"
 import Login from "./pages/Login"
+import ProtectedRoute from "../src/componenets/ProtectedRoute"
 import "./App.css"
 
 function App() {
@@ -7,6 +9,14 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+         <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
---
+++ employe-management-dashboard/src/componenets/ProtectedRoute.jsx
@@ -0,0 +1,11 @@
+import {Navigate} from 'react-router-dom'
+import { isAuthenticated } from '../utils/auth'
+
+const ProtectedRoute = ({ children }) => {
+    if(!isAuthenticated()){
+        return <Navigate to = "/" />
+    }
+    return children
+}
+
+export default ProtectedRoute
